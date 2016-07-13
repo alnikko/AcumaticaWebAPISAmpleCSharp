ee9b6016656b2cb6624dd1490eff34301e35f91e
alnikko <nikkgeek@outlook.com>
2016-07-13 10:43:50 +0800

    Initial Commit

---
+++ AcumaticaWebServiceSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcumaticaWebServiceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void CreateCustomer()
+        {
+            
+        }
+
+    }
+}
